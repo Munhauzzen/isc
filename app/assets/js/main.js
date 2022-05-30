dde027defaa53dddb9d0d4a805f8943e3eb54518
Munhauzzen <dvostok1337@gmail.com>
2022-05-30 22:05:19 +0300

    finish

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,5 +1,8 @@
-$(function(){
-
-
-
-});
+const burgerMenu = document.querySelector('.menu-btn');
+if (burgerMenu) {
+  const bodyMenu = document.querySelector('.header-menu');
+  burgerMenu.addEventListener("click", function (e) {
+    burgerMenu.classList.toggle('active');
+    bodyMenu.classList.toggle('active');
+  });
+}  
