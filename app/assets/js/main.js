14f12330e61b273e110de9936800817731c75a49
Munhauzzen <dvostok1337@gmail.com>
2022-05-31 00:10:33 +0300

    fix

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,8 +1,36 @@
 const burgerMenu = document.querySelector('.menu-btn');
+const bodyMenu = document.querySelector('.header-menu');
 if (burgerMenu) {
-  const bodyMenu = document.querySelector('.header-menu');
   burgerMenu.addEventListener("click", function (e) {
     burgerMenu.classList.toggle('active');
     bodyMenu.classList.toggle('active');
   });
-}  
+}
+
+
+
+const menuLinks = document.querySelectorAll('.header-menu__link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector("header").offsetHeight;
+
+      if (burgerMenu.classList.contains('active')) {
+        burgerMenu.classList.remove('active');
+        bodyMenu.classList.remove('active');
+      }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    }
+  }
+}
